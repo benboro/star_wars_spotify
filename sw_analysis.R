38bb6734002db963b24e08f4752bef1dfeb5048b
Ben Borovinsky <bmborovinsky@gmail.com>
2020-10-21 00:05:26 -0700

    Add files via upload

--- sw_analysis.R
+++ sw_analysis.R
@@ -2,6 +2,7 @@ library(dplyr, quietly = TRUE)
 #library(dplyr, warn.conflicts = FALSE)
 library(GGally, quietly = TRUE)
 library(fmsb)
+library(tidyr)
 #library(ggthemes) # for FiveThirtyEight-style plot
 
 # CLEAR WORKSPACE AND PLOT WINDOW
@@ -103,14 +104,60 @@ ggparcoord(df,
 # WRITE DATA TO CSV
 #write.csv(song_data,"./data/song_data.csv", row.names = FALSE)
 
-# t_sd <- song_data %>%
-#   reshape2::melt(id.vars=c("movie_title", "trilogy", "track_name"), measure.vars=categories)
-# t_od <- og_data %>%
-#   reshape2::melt(id.vars=c("movie_title", "trilogy", "track_name"), measure.vars=categories)
-# sd2 <- song_data
-# sd2$id <- 1:nrow(song_data)
-# t_sd2 <- sd2 %>%
-#   pivot_longer(cols = categories, 'name', 'value')
-# 
-# ggplot(t_sd2) +
-#   geom_line(aes(x=name, y=value, group=id))
+sd <- song_data
+#sd$id <- 1:nrow(song_data)
+t_sd <- sd %>%
+  pivot_longer(cols = categories, 'name', 'value') %>%
+  select("movie_title", "trilogy", "track_name", "name", "value")
+t_sd[names(t_sd) != "value"] <- t_sd[names(t_sd) != "value"] %>%
+  {lapply(., function(x) {factor(x, levels=unique(x))})}
+o_sd <- t_sd %>%
+  filter(trilogy=="Original")
+
+movie_colors <- c("A New Hope"="#2E66B1", 
+               "The Empire Strikes Back"="#E13028",
+               "Return of the Jedi"="#188655", 
+               "The Phantom Menace"="#B7ADB0", 
+               "Attack of the Clones"="#D79E2A", 
+               "Revenge of the Sith"="#8956A3", 
+               "The Force Awakens"="#E9D84F", 
+               "The Last Jedi"="#C60B36", 
+               "The Rise of Skywalker"="#49508D")
+track_colors <- c("#2E66B1", "#E13028", "#188655", 
+                  "#B7ADB0", "#D79E2A", "#8956A3", 
+                  "#E9D84F", "#C60B36", "#49508D")
+
+# g_back <- ggplot() +
+#   geom_line(aes(x=name, y=value, group=track_name, color=movie_title), 
+#             data=t_sd, alpha=0.2, #color="grey",
+#             show.legend=FALSE) +
+#   scale_color_manual(values=rep(sw_colors))
+# g_back
+# g_dynamic <- ggplot() +
+#   geom_line(aes(x=name, y=value, group=track_name, color=movie_title), 
+#             data=o_sd, alpha=1.0, lwd=1, 
+#             show.legend=TRUE) +
+#   scale_color_manual(values=rep(sw_colors, length.out=3))
+# g_dynamic
+
+ggplot() +
+  geom_line(aes(x=name, y=value, group=track_name, color=movie_title), 
+            data=t_sd, alpha=1.0, color="grey",
+            show.legend=FALSE) +
+  geom_line(aes(x=name, y=value, group=track_name, color=movie_title), 
+            data=o_sd, alpha=0.5, lwd=1, 
+            show.legend=TRUE) +
+  scale_color_manual(values=rep(movie_colors, length.out=9)) +
+  guides(color=guide_legend(override.aes = list(size=1, alpha=1.0))) +
+  theme_minimal() +
+  coord_cartesian(ylim = c(0, 1)) + 
+  scale_x_discrete(expand = c(0,0)) +
+  labs(x="",y="strength") +
+  theme(text = element_text(size = 16),
+        panel.background = element_rect(fill = "transparent", color = NA),
+        plot.margin = unit(c(1, 1, 1.5, 1.2), "cm"),
+        legend.title = element_blank(),
+        legend.text = element_text(size=10),
+        #legend.key.width = unit(1, "cm"),
+        axis.text.x = element_text(angle = 25, vjust = 0.7, hjust=0.7))
+
