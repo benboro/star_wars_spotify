2c99822b1c767f44f4db0805ab7746a03e984a6a
Ben Borovinsky <bmborovinsky@gmail.com>
2020-10-22 17:09:42 -0700

    Add files via upload

--- sw_analysis.R
+++ sw_analysis.R
@@ -1,8 +1,9 @@
 library(dplyr, quietly = TRUE)
 #library(dplyr, warn.conflicts = FALSE)
-library(GGally, quietly = TRUE)
-library(fmsb)
+#library(GGally, quietly = TRUE)
+#library(fmsb)
 library(tidyr)
+library(ggplot2)
 #library(ggthemes) # for FiveThirtyEight-style plot
 
 # CLEAR WORKSPACE AND PLOT WINDOW
@@ -77,21 +78,21 @@ cat_col <- which(colnames(song_data) %in% categories)
 
 df <- og_data[og_data$episode==4,]
 
-ggparcoord(df,
-  columns=cat_col,
-  groupColumn=ifelse(length(unique("movie_title"))==1,"track_name","movie_title"),
-  scale="globalminmax",
-  showPoints=FALSE,
-  alphaLines=1
-) +
-  theme_minimal() +
-  scale_x_discrete(expand = c(0,0)) +
-  # scale_color_manual(values=c("#2E66B1", "#E13028", "#188655", 
-  #                             "#B7ADB0", "#D79E2A", "#8956A3", 
-  #                             "#E9D84F", "#C60B36", "#49508D")) +
-  labs(x="",y="strength") +
-  theme(text = element_text(size = 16),
-        legend.title = element_blank())
+# ggparcoord(df,
+#   columns=cat_col,
+#   groupColumn=ifelse(length(unique("movie_title"))==1,"track_name","movie_title"),
+#   scale="globalminmax",
+#   showPoints=FALSE,
+#   alphaLines=1
+# ) +
+#   theme_minimal() +
+#   scale_x_discrete(expand = c(0,0)) +
+#   # scale_color_manual(values=c("#2E66B1", "#E13028", "#188655", 
+#   #                             "#B7ADB0", "#D79E2A", "#8956A3", 
+#   #                             "#E9D84F", "#C60B36", "#49508D")) +
+#   labs(x="",y="strength") +
+#   theme(text = element_text(size = 16),
+#         legend.title = element_blank())
 
 # radar_data <- rbind(rep(1,ncol(og_data)),rep(0,ncol(og_data)),og_data)
 # radarchart(radar_data[,10:16], axistype = 4,
@@ -112,7 +113,7 @@ t_sd <- sd %>%
 t_sd[names(t_sd) != "value"] <- t_sd[names(t_sd) != "value"] %>%
   {lapply(., function(x) {factor(x, levels=unique(x))})}
 o_sd <- t_sd %>%
-  filter(trilogy=="Original")
+  filter(movie_title=="Attack of the Clones")
 
 movie_colors <- c("A New Hope"="#2E66B1", 
                "The Empire Strikes Back"="#E13028",
@@ -144,10 +145,10 @@ ggplot() +
   geom_line(aes(x=name, y=value, group=track_name, color=movie_title), 
             data=t_sd, alpha=1.0, color="grey",
             show.legend=FALSE) +
-  geom_line(aes(x=name, y=value, group=track_name, color=movie_title), 
-            data=o_sd, alpha=0.5, lwd=1, 
+  geom_line(aes(x=name, y=value, group=track_name, color=track_name), 
+            data=o_sd, alpha=0.5, lwd=1.5, 
             show.legend=TRUE) +
-  scale_color_manual(values=rep(movie_colors, length.out=9)) +
+  scale_color_manual(values=rep(track_colors, length.out=44)) +
   guides(color=guide_legend(override.aes = list(size=1, alpha=1.0))) +
   theme_minimal() +
   coord_cartesian(ylim = c(0, 1)) + 
@@ -155,9 +156,10 @@ ggplot() +
   labs(x="",y="strength") +
   theme(text = element_text(size = 16),
         panel.background = element_rect(fill = "transparent", color = NA),
-        plot.margin = unit(c(1, 1, 1.5, 1.2), "cm"),
+        plot.margin = unit(c(0.5, 2.0, 0.5, 0.5), "cm"),
         legend.title = element_blank(),
         legend.text = element_text(size=10),
         #legend.key.width = unit(1, "cm"),
+        axis.text = element_text(color = "black"),
         axis.text.x = element_text(angle = 25, vjust = 0.7, hjust=0.7))
 
